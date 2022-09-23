cb34cb9f4f82ca6d2b196657354573957d745784
Adriana Echeverri <adriana.echeverriromero@mercadolibre.com.co>
2022-09-23 12:34:13 -0500

    Gobase1 files

---
+++ gobase1/exercise3.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	var nombre1 string //no debia empezar con un numero
+	var apellido string
+	var edad int //Estaba al reves el nombre y tipo
+	apellido1 := 6 //no debia empezar con un numero
+	var licenciaDeConducir = true // debe estar en camel case
+	var estaturaDeLaPersona int //no colocar espacios
+	cantidadDeHijos := 2
+}
